929f121a22869ba5e11e26205de7a8b2c1c1a168
Yuliia Komlyk <juliajuli99933@gmail.com>
2025-01-23 02:48:48 +0200

    Added code for the tasks 1-4

---
+++ HW3/script.js
@@ -0,0 +1,57 @@
+// //Task #1
+// let name = prompt("What is your name?");
+// alert(`Hello, ${name}. How are you?`)
+
+
+// //Task #2
+// let num_1 = Number(prompt("Please enter the FIRST number."));
+// let num_2 = Number(prompt("Please enter the SECOND number."));
+
+// let sum = num_1 + num_2;
+// let difference = num_1 - num_2;
+// let multiplication = num_1 * num_2;
+// let division = num_2 !== 0 ? num_1 / num_2 : "Division by zero is not allowed.";
+
+// alert(`Entered data:
+//     The FIRST number is: ${num_1}.
+//     The SECOND number is: ${num_2}.
+
+//     The calculated result is:
+//     Sum: ${sum}.
+//     Difference: ${difference}.
+//     Multiplication: ${multiplication}.
+//     Division: ${division}.`);
+
+
+// //Task #3
+// let value_1 = prompt("Comparison of values. Please enter the FIRST value.");
+// let value_2 = prompt("Comparison of values. Please enter the SECOND value.");
+
+// let comparison = (value_1 == value_2);
+// let comparisonResult = comparison.toString().toUpperCase();
+
+// alert(`Entered data:
+//     The FIRST value is: ${value_1}.
+//     The SECOND value is: ${value_2}.
+
+//     The comparison result is: ${comparisonResult}.`);
+
+
+// // Task #4
+// let digit_1 = Number(prompt("Please enter the FIRST number."));
+// let digit_2 = Number(prompt("Please enter the SECOND number."));
+// let digit_3 = Number(prompt("Please enter the THIRD number."));
+
+// let mean = (digit_1 + digit_2 + digit_3) / 3;
+
+// alert(`Entered data:
+//     The FIRST number is: ${digit_1}.
+//     The SECOND number is: ${digit_2}.
+//     The THIRD number is: ${digit_3}.
+    
+//     The arithmetic mean result is: ${mean}.`);
+
+
+//Task #5
+let number = 98765;
+

857f3c8636c171bb44a53727795e10b70146902a
Yuliia Komlyk <juliajuli99933@gmail.com>
2025-01-26 19:43:11 +0200

    Added hw 6

--- HW 1/code.js
+++ HW 1/code.js
@@ -1,2 +1,19 @@
-console.log('Hello world!');
-console.log('Hello world!');
+let numOrStr = prompt('input number or string');
+console.log(numOrStr);
+
+switch (true) {
+    case numOrStr === null:
+        console.log('ви відмінили');
+        break;
+
+    case numOrStr.trim() === '':
+        console.log('Empty String');
+        break;
+
+    case isNaN(+numOrStr):
+        console.log('number is Ba_NaN');
+        break;
+        
+    default:
+        console.log('OK!');
+}

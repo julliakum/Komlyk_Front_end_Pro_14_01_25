d4c3ed30f858ac4b4325df04ef7a032c63f18d50
Yuliia Komlyk <juliajuli99933@gmail.com>
2025-01-29 02:05:54 +0200

    Added hw

---
+++ HW 1/script.js
@@ -0,0 +1,40 @@
+//Way1
+let year = prompt("How old are you?");
+let city = prompt("What city do you live in?");
+let sport = prompt("What is your favorite sport?");
+
+let capitalCities = {
+    "Kyiv": "Ukraine",
+    "Washington": "the United States",
+    "London": "Great Britain"
+};
+
+let sportsChampions = {
+    "Football": "Lionel Messi",
+    "Basketball": "LeBron James",
+    "High jump": "Yaroslava Mahuchikh"
+};
+
+if (!year) {
+    year = "Sorry, you didn’t want to enter your age.";
+} else {
+    year = `Your age: ${year}.`;
+}
+
+if (!city) {
+    city = "Sorry, you didn’t want to enter your city.";
+} else if (capitalCities[city]) {
+    city = `You live in the capital of ${capitalCities[city]}.`;
+} else {
+    city = "You live in a very beautiful city!";
+}
+
+if (!sport) {
+    sport = "Sorry, you didn’t want to enter your favorite sport.";
+} else if (sportsChampions[sport]) {
+    sport = `Cool! Do you want to become like ${sportsChampions[sport]} in ${sport}?`;
+} else {
+    sport = "Wow! I’m sure you’ll be great at this sport!";
+}
+
+alert(`${year} \n${city} \n${sport}`);

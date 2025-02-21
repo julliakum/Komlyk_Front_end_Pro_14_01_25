4d3905ffb9c66e62946fd6439dabf116ceee56c6
Yuliia Komlyk <juliajuli99933@gmail.com>
2025-02-21 22:51:33 +0200

    Added hw12

--- HW 1/script.js
+++ HW 1/script.js
@@ -1,2 +1,13 @@
-console.log('Hello world!');
+// Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
+function removeElement(array, item) {
+    for (let i = array.length - 1; i >= 0; i--) {
+        if (array[i] === item) {
+            array.splice(i, 1);
+        }
+    }
+}
 
+const array = [1, 2, 3, 4, 5, 5, 6, 7];
+removeElement(array, 5);
+
+console.log(array);

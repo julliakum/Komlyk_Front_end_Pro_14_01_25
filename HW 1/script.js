58cec9e4504bc3fa8dbe6c7a4be2897bda05d424
Yuliia Komlyk <juliajuli99933@gmail.com>
2025-03-07 00:36:12 +0200

    Added hw15

--- HW 1/script.js
+++ HW 1/script.js
@@ -1,2 +1,18 @@
-console.log('Hello world!');
+// Реалізувати рекурсивну функцію, яка зводить число в ступінь.
+// Число, яке потрібно звести в ступінь, передається як перший аргумент у функцію
+// Ступінь передається як другий аргумент у функцію
+// pow(num, degree)
 
+function pow(num, degree) {
+    if (degree === 0) return 1;
+    if (degree < 0) return 1 / pow(num, -degree);
+
+    degree--;
+    return num * pow(num, degree);}
+
+const number = parseFloat(prompt("Enter the number:"));
+const exponent = parseInt(prompt("Enter the degree:"));
+
+console.log(`The number: ${number}, 
+The degree: ${exponent}, 
+Result: ${pow(number, exponent)}`);

bdfab9e1a75979d252503e2eeca717438d78643f
Yuliia Komlyk <juliajuli99933@gmail.com>
2025-03-18 00:45:31 +0200

    HW added

--- HW 1/script.js
+++ HW 1/script.js
@@ -1,2 +1,38 @@
-console.log('Hello world!');
+// Метод 1 якщо треба в результаті просто робити виклик по ланцюжку
+let ladder = {
+    step: 0,
+    up: function () {
+        this.step++;
+        return this;
+    },
+    down: function () {
+        this.step--;
+        return this;
+    },
+    showStep: function () {
+        alert(`Метод №1: ${this.step}`);
+        return this;
+    }
+};
 
+ladder.up().up().down().showStep(); // 1
+
+
+// Метод 2 якщо треба в передавати кількість кроків (аргументи) 
+let ladder2 = {
+    step: 0,
+    up: function (n = 1) {
+        this.step += n;
+        return this;
+    },
+    down: function (n = 1) {
+        this.step -= n;
+        return this;
+    },
+    showStep: function () {
+        alert(`Метод №2: ${this.step}`);
+        return this;
+    }
+};
+
+ladder2.up(5).up(1).down(2).showStep(); // 4

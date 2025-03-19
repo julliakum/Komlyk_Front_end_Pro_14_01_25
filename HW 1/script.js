3a6243a3d3458f1b1b5afcdd59f8aa5fed870f5b
Yuliia Komlyk <juliajuli99933@gmail.com>
2025-03-19 23:17:27 +0200

    Vote for your favorite emoji anв have fun!

--- HW 1/script.js
+++ HW 1/script.js
@@ -1,2 +1,13 @@
 console.log('Hello world!');
 
+document.addEventListener("DOMContentLoaded", () => {
+    const votes = [0, 0, 0, 0, 0];
+    const buttons = document.querySelectorAll(".emoji-button");
+    buttons.forEach(button => {
+      button.addEventListener("click", () => {
+        const index = button.getAttribute("data-index");
+        votes[index]++;
+        document.getElementById(`count-${index}`).textContent = votes[index];
+      });
+    });
+  });

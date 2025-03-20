6f41c746929b29a942fb0f8c32ac4e28dd59172f
Yuliia Komlyk <juliajuli99933@gmail.com>
2025-03-21 01:18:23 +0200

    Added generateList

--- HW 1/script.js
+++ HW 1/script.js
@@ -1,2 +1,24 @@
-console.log('Hello world!');
+function generateList(array) {
+    const ul = document.createElement('ul');
 
+    array.forEach(item => {
+        const li = document.createElement('li');
+
+        if (Array.isArray(item)) {
+            li.appendChild(generateList(item));
+        } else {
+            li.textContent = item;
+        }
+
+        ul.appendChild(li);
+    });
+
+    return ul;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const data = [1, 2, [1.1, 1.2, 1.3], 3, [3.1, 3.2, 3.3]];
+    const wrapper = document.querySelector('.wrapper');
+    const list = generateList(data);
+    wrapper.appendChild(list);
+});

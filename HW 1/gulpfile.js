48fb12c10c6e499dbb6e0ffb3aa08798118584f4
Yuliia Komlyk <juliajuli99933@gmail.com>
2025-05-06 02:27:34 +0300

    Updated hw35 - and ad HW 37

--- HW 1/gulpfile.js
+++ HW 1/gulpfile.js
@@ -5,23 +5,21 @@ const cleanCSS = require("gulp-clean-css");
 const babel = require("gulp-babel");
 const minify = require("gulp-minify");
 
-// JS task -> concat + babel + minify
 task("js", () => {
   return src("src/*.js")
-    .pipe(concat("main.js"))          // объединяем
+    .pipe(concat("main.js")) 
     .pipe(babel({
-      presets: ["@babel/preset-env"]  // транспиляция в ES5
+      presets: ["@babel/preset-env"]
     }))
     .pipe(minify({
       ext: {
-        min: ".min.js"                // имя файла после минификации -> main.min.js
+        min: ".min.js"
       },
-      noSource: true                  // не оставлять обычный main.js (если хочешь оставить - убери эту строку)
+      noSource: true
     }))
     .pipe(dest("dist"));
 });
 
-// SCSS task -> sass + concat + minify
 task("scss", () => {
   return src("src/*.scss")
     .pipe(sass().on("error", sass.logError))
@@ -30,11 +28,9 @@ task("scss", () => {
     .pipe(dest("dist"));
 });
 
-// Watch task -> следим за файлами
 task("watch", () => {
   watch("src/*.js", series("js"));
   watch("src/*.scss", series("scss"));
 });
 
-// Default -> build + watch
 task("default", series("js", "scss", "watch"));
